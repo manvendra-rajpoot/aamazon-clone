d3f86a660d7644ac4abd7b280a3d491ba7417aab
manvendra-rajpoot <manvendra141986@gmail.com>
2021-01-04 21:48:01 +0530

    header modified :rocket:

--- src/Header.js
+++ src/Header.js
@@ -2,18 +2,27 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 function Header() {
     return (
         <div className='header'>
             <img className='header__logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='aamazon' />
 
-            <div className="header__search">
-                <input className='header__searchInput' type='text' />
-                <SearchIcon className='header__searchIcon' />
-            </div>
-
             <div className="header__nav">
+                <LocationOnIcon className=' header__option header__locationIcon' style={{marginRight:'-15px',marginLeft:'-10px'}} />
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Hello
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Select your address
+                    </span>
+                </div>
+
+                <Searchbar />
+
                 <div className="header__option">
                     <span className="header__optionLineOne">
                         Hello, Sign in
@@ -42,7 +51,7 @@ function Header() {
                 </div>
 
                 <ShoppingCartIcon className="header__option" />
-                <div className="header__optionCart">
+                <div className="header__option" style={{marginLeft:'-6px'}}>
                     <span className="header__optionLineTwo header__orderCounts">
                         4
                     </span>
@@ -52,6 +61,15 @@ function Header() {
                 </div>
             </div>
             
+        </div>
+    )
+}
+
+const Searchbar = () => {
+    return (
+        <div className="header__search">
+            <input className='header__searchInput' type='text' />
+            <SearchIcon className='header__searchIcon' />
         </div>
     )
 }
