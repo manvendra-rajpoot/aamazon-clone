7f098ca985f49825823f3f4cb511ee10d601b214
manvendra-rajpoot <manvendra141986@gmail.com>
2021-01-04 20:28:05 +0530

    header ui :rocket:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>AMAZON CLONE!</h1>
+      <Header />
+      {/* <Home /> */}
+
     </div>
   );
 }
